5232d1a34f044160c780107d0d433ded31bccfc9
Vincent Yang <missuo@pm.me>
2024-11-20 18:24:19 -0500

    fix: icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="icon" href="/icons/folder.png" />
+          <link rel="icon" href="/icons/forward.png" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
           {siteConfig.googleFontLinks.map(link => (
