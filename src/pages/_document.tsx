ada8779d25226257b03ffc6c79278d715784dbb5
Vincent Yang <missuo@pm.me>
2024-11-20 18:12:56 -0500

    fix: favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,14 @@
+/*
+ * @Author: Vincent Yang
+ * @Date: 2024-11-20 17:41:16
+ * @LastEditors: Vincent Yang
+ * @LastEditTime: 2024-11-20 18:12:52
+ * @FilePath: /onedrive-cf-index-ng/src/pages/_document.tsx
+ * @Telegram: https://t.me/missuo
+ * @GitHub: https://github.com/missuo
+ * 
+ * Copyright © 2024 by Vincent, All Rights Reserved. 
+ */
 import Document, { Head, Html, Main, NextScript } from 'next/document'
 import siteConfig from '../../config/site.config'
 
@@ -7,7 +18,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta name="description" content="OneDrive Vercel Index" />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/icons/folder.png" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
           {siteConfig.googleFontLinks.map(link => (
